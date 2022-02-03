d61426834d3bf57824c99620aedb38ffba24c929
aramsichord@gmail.com <aramsichord@gmail.com>
2022-02-03 08:33:46 -0700

    todo 1-3 done

--- materials/lab/2/bhg-scanner/scanner/scanner.go
+++ materials/lab/2/bhg-scanner/scanner/scanner.go
@@ -3,6 +3,7 @@
 // License: {$RepoRoot}/materials/BHG-LICENSE
 // Useage:
 // {TODO 1: FILL IN}
+// run: go test
 
 package scanner
 
@@ -10,17 +11,18 @@ import (
 	"fmt"
 	"net"
 	"sort"
+	"time"
 )
 
 //TODO 3 : ADD closed ports; currently code only tracks open ports
-var openports []int  // notice the capitalization here. access limited!
-
+var openports []int // notice the capitalization here. access limited!
+var closedports []int
 
 func worker(ports, results chan int) {
 	for p := range ports {
-		address := fmt.Sprintf("scanme.nmap.org:%d", p)    
-		conn, err := net.Dial("tcp", address) // TODO 2 : REPLACE THIS WITH DialTimeout (before testing!)
-		if err != nil { 
+		address := fmt.Sprintf("scanme.nmap.org:%d", p)
+		conn, err := net.DialTimeout("tcp", address, 3000*time.Millisecond) // TODO 2 : REPLACE THIS WITH DialTimeout (before testing!)
+		if err != nil {
 			results <- 0
 			continue
 		}
@@ -32,11 +34,11 @@ func worker(ports, results chan int) {
 // for Part 5 - consider
 // easy: taking in a variable for the ports to scan (int? slice? ); a target address (string?)?
 // med: easy + return  complex data structure(s?) (maps or slices) containing the ports.
-// hard: restructuring code - consider modification to class/object 
+// hard: restructuring code - consider modification to class/object
 // No matter what you do, modify scanner_test.go to align; note the single test currently fails
-func PortScanner() int {  
+func PortScanner() (int, int) {
 
-	ports := make(chan int, 100)   // TODO 4: TUNE THIS FOR CODEANYWHERE / LOCAL MACHINE
+	ports := make(chan int, 100) // TODO 4: TUNE THIS FOR CODEANYWHERE / LOCAL MACHINE
 	results := make(chan int)
 
 	for i := 0; i < cap(ports); i++ {
@@ -53,19 +55,25 @@ func PortScanner() int {
 		port := <-results
 		if port != 0 {
 			openports = append(openports, port)
+		} else {
+			closedports = append(closedports, port)
 		}
 	}
 
 	close(ports)
 	close(results)
 	sort.Ints(openports)
+	sort.Ints(closedports)
 
 	//TODO 5 : Enhance the output for easier consumption, include closed ports
 
 	for _, port := range openports {
 		fmt.Printf("%d open\n", port)
 	}
+	for _, port := range closedports {
+		fmt.Printf("%d closed\n", port)
+	}
 
-	return len(openports) // TODO 6 : Return total number of ports scanned (number open, number closed); 
+	return len(openports), len(closedports) // TODO 6 : Return total number of ports scanned (number open, number closed);
 	//you'll have to modify the function parameter list in the defintion and the values in the scanner_test
 }
