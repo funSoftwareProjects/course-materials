2c6e400c062aac238f3784e5fcf2541399e9e491
aramsichord@gmail.com <aramsichord@gmail.com>
2022-03-09 12:44:28 -0700

    doing stuff

--- materials/lab/4/wyoassign/wyoassign.go
+++ materials/lab/4/wyoassign/wyoassign.go
@@ -5,26 +5,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
 	"strconv"
 
+	"github.com/gorilla/mux"
 )
 
-type Response struct{
+type Response struct {
 	Assignments []Assignment `json:"assignments"`
 }
 
 type Assignment struct {
-	Id string `json:"id"`
-	Title string `json:"title`
+	Id          string `json:"id"`
+	Title       string `json:"title`
 	Description string `json:"desc"`
-	Points int `json:"points"`
+	Points      int    `json:"points"`
 }
 
 var Assignments []Assignment
+
 const Valkey string = "FooKey"
 
-func InitAssignments(){
+func InitAssignments() {
 	var assignmnet Assignment
 	assignmnet.Id = "Mike1A"
 	assignmnet.Title = "Lab 4 "
@@ -39,7 +40,6 @@ func APISTATUS(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "API is up and running")
 }
 
-
 func GetAssignments(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Entering %s end point", r.URL.Path)
 	var response Response
@@ -48,14 +48,14 @@ func GetAssignments(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	
+
 	jsonResponse, err := json.Marshal(response)
 
 	if err != nil {
 		return
 	}
 
-	//TODO 
+	//TODO
 	w.Write(jsonResponse)
 }
 
@@ -66,7 +66,7 @@ func GetAssignment(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 
 	for _, assignment := range Assignments {
-		if assignment.Id == params["id"]{
+		if assignment.Id == params["id"] {
 			json.NewEncoder(w).Encode(assignment)
 			break
 		}
@@ -80,18 +80,18 @@ func DeleteAssignment(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/txt")
 	w.WriteHeader(http.StatusOK)
 	params := mux.Vars(r)
-	
+
 	response := make(map[string]string)
 
 	response["status"] = "No Such ID to Delete"
 	for index, assignment := range Assignments {
-			if assignment.Id == params["id"]{
-				Assignments = append(Assignments[:index], Assignments[index+1:]...)
-				response["status"] = "Success"
-				break
-			}
+		if assignment.Id == params["id"] {
+			Assignments = append(Assignments[:index], Assignments[index+1:]...)
+			response["status"] = "Success"
+			break
+		}
 	}
-		
+
 	jsonResponse, err := json.Marshal(response)
 	if err != nil {
 		return
@@ -102,12 +102,23 @@ func DeleteAssignment(w http.ResponseWriter, r *http.Request) {
 func UpdateAssignment(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Entering %s end point", r.URL.Path)
 	w.Header().Set("Content-Type", "application/json")
-	
+
 	var response Response
 	response.Assignments = Assignments
 
+	DeleteAssignment(w, r)
 
-
+	var updatedAssignment Assignment
+	r.ParseForm()
+	if r.FormValue("id") != "" {
+		updatedAssignment.Id = r.FormValue("id")
+		updatedAssignment.Title = r.FormValue("title")
+		updatedAssignment.Description = r.FormValue("desc")
+		updatedAssignment.Points, _ = strconv.Atoi(r.FormValue("points"))
+		Assignments = append(Assignments, updatedAssignment)
+		w.WriteHeader(http.StatusCreated) //change to status updated
+	}
+	w.WriteHeader(http.StatusNotFound)
 }
 
 func CreateAssignment(w http.ResponseWriter, r *http.Request) {
@@ -116,11 +127,11 @@ func CreateAssignment(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	// Possible TODO: Better Error Checking!
 	// Possible TODO: Better Logging
-	if(r.FormValue("id") != ""){
-		assignmnet.Id =  r.FormValue("id")
-		assignmnet.Title =  r.FormValue("title")
-		assignmnet.Description =  r.FormValue("desc")
-		assignmnet.Points, _ =  strconv.Atoi(r.FormValue("points"))
+	if r.FormValue("id") != "" {
+		assignmnet.Id = r.FormValue("id")
+		assignmnet.Title = r.FormValue("title")
+		assignmnet.Description = r.FormValue("desc")
+		assignmnet.Points, _ = strconv.Atoi(r.FormValue("points"))
 		Assignments = append(Assignments, assignmnet)
 		w.WriteHeader(http.StatusCreated)
 	}
